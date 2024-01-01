8240586e2576737d7e33412a2b8c7723f4fb90f3
Jitendra Gupta <jitendragupta@Jitendras-MacBook-Air.local>
2024-01-01 20:12:57 +0530

    removed content gen file

--- pkg/explain.go
+++ pkg/explain.go
@@ -1,9 +1,112 @@
 package pkg
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
+
+const gptAPIURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
+
+type RequestBody struct {
+	Contents []struct {
+		Parts []struct {
+			Text string `json:"text"`
+		} `json:"Parts"`
+	} `json:"contents"`
+}
+
+type ResponseBody struct {
+	Candidates []struct {
+		Content struct {
+			Parts []struct {
+				Text string `json:"text"`
+			} `json:"parts"`
+			Role string `json:"role"`
+		} `json:"content"`
+		FinishReason  string `json:"finishReason"`
+		Index         int    `json:"index"`
+		SafetyRatings []struct {
+			Category    string `json:"category"`
+			Probability string `json:"probability"`
+		} `json:"safetyRatings"`
+	} `json:"candidates"`
+	PromptFeedback struct {
+		SafetyRatings []struct {
+			Category    string `json:"category"`
+			Probability string `json:"probability"`
+		} `json:"safetyRatings"`
+	} `json:"promptFeedback"`
+}
+
+func GenerateContent(inputText string) (string, error) {
+	apiKey := os.Getenv("GEMINI_API_KEY")
+	if apiKey == "" {
+		return "", fmt.Errorf("API Key not set")
+	}
+
+	requestBody := RequestBody{
+		Contents: []struct {
+			Parts []struct {
+				Text string `json:"text"`
+			} `json:"Parts"`
+		}{
+			{
+				Parts: []struct {
+					Text string `json:"text"`
+				}{
+					{Text: inputText},
+				},
+			},
+		},
+	}
+
+	requestBodyBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal request body: %v", err)
+	}
+
+	resp, err := http.Post(gptAPIURL, "application/json", bytes.NewBuffer(requestBodyBytes))
+
+	if err != nil {
+		return "", fmt.Errorf("failed to make API request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	var response ResponseBody
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal response body: %v", err)
+	}
+
+	if len(response.Candidates) > 0 && len(response.Candidates[0].Content.Parts) > 0 {
+		return response.Candidates[0].Content.Parts[0].Text, nil
+	}
+
+	return "", fmt.Errorf("no generated text found in response")
+}
+
+func GenerateContentAndSave(inputText, filePath string) error {
+	generatedText, err := GenerateContent(inputText)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filePath, []byte(generatedText), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %v", err)
+	}
+
+	return nil
+}
 
 // AnalyzeTerraform reads and analyzes a Terraform file.
 func AnalyzeTerraform(filePath string) (string, error) {
